1b1da88fcf771c7a49911689bebdf153dfa3aba2
Jon Redeker <jon.redeker@gmail.com>
2023-05-08 17:30:20 -0400

    fix issues with setup sh script for linux machines

--- linux/setup.sh
+++ linux/setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # This is to be run ONCE after a new linux box is set up.
 # Additional setup will be needed to have all user vars set and keys generated
 
@@ -6,10 +8,10 @@ sudo apt update
 sudo apt upgrade -y
 
 # ZSH
-if [ "$NO_ZSH" != "true" ]; then
+if [[ "${NO_ZSH}" != "true" ]]; then
   # switch to zsh
   sudo apt install zsh -y
-  sudo chsh gitpod -s "$(which zsh)"
+  sudo chsh gitpod -s "$(command -v zsh || true)"
 
   # Fix GP alias
   echo "alias pod=/usr/bin/gp" >>~/.zshrc
@@ -17,25 +19,25 @@ if [ "$NO_ZSH" != "true" ]; then
   # install ohmyzsh
   CHSH=no
   export CHSH # fix broken chsh check
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh || true)" "" --unattended
 
   # install plugins
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
   echo "plugins+=zsh-autosuggestions" >>~/.zshrc
-  git clone git clone https://github.com/ptavares/zsh-direnv.git ${ZSH_CUSTOM: ~/.oh-my-zsh/custom}/plugins/zsh-direnv
-  echo "plugins+=(zsh-direnv)" >>~/.zshrc
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  git clone git clone https://github.com/ptavares/zsh-direnv.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-direnv
+  echo "plugins+=zsh-direnv" >>~/.zshrc
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
   echo "plugins+=zsh-syntax-highlighting" >>~/.zshrc
-  git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+  git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-completions
   echo "fpath+=${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions/src" >>~/.zshrc
 
   # install powerlevel10k
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}"/themes/powerlevel10k
   #curl -fsSL https://raw.githubusercontent.com/DRSchlaubi/.gitpod/main/.p10k.zsh -o ~/.p10k.zsh
   echo ZSH_THEME="powerlevel10k/powerlevel10k" >>~/.zshrc
 
   # install direnv
-  curl -sfL https://direnv.net/install.sh | bash
+  curl -sfL https://direnv.net/install.sh | bash || true
 
   # reload oh-my-zsh instructions again
   echo "source \$ZSH/oh-my-zsh.sh" >>~/.zshrc
